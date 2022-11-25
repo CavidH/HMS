f17ff3af5c801d6a96c149300f51a66f6205169d
hcavid386@gmail.com <hcavid386@gmail.com>
2022-11-25 18:02:12 +0400

    :bomb:

---
+++ HMS/Controllers/DoctorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using HMS.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.Controllers
+{
+    [Authorize]
+    public class DoctorController : Controller
+    {
+        private readonly IUnitOfWorkService _unitOfWorkService;
+
+        public DoctorController(IUnitOfWorkService unitOfWorkService)
+        {
+            _unitOfWorkService = unitOfWorkService;
+        }
+
+        public async Task<IActionResult> Index( /*int page = 1*/)
+        {
+            return View(await _unitOfWorkService.DoctorService.GetAllAsync());
+        }
+
+        public async Task<IActionResult> AddDoctor(int id)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await _unitOfWorkService.DoctorPatientService.CreateAsync(userId, id);
+            ViewBag.msg = "Added";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
